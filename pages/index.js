02d903a5114472a8b1fc42cda008ebb408d871e9
mauvetech <micson774@gmail.com>
2022-06-18 21:53:15 +0200

    Test Text

--- pages/index.js
+++ pages/index.js
@@ -238,7 +238,21 @@ export default function Home() {
                 </div>
               </div>
             </header>
-            <h1>C'est le About</h1>
+            <div className="text-white text-xl ml-[54vw] mb-[22vh]  absolute  w-1/3 h-1/5">
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+              </p>
+            </div>
+
+            <Image
+              src="/AVENSTUDIO_GROS_LOGO3.png"
+              layout="fill"
+              objectFit="cover"
+            />
           </div>
         ) : null}
         {menu == "Contact" ? (
