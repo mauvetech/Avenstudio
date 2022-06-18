344d278646b93aa2923cdd2e2e1d6ee705aee787
mauvetech <micson774@gmail.com>
2022-06-18 22:43:20 +0200

    Reunion officiel Trezzz

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,23 @@ module.exports = {
         cursive: ["Lobster"],
       },
     },
+
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "2536px",
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [require("tailwind-scrollbar-hide")],
 };
