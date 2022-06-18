3ac32de8f63a64ab1b1d31b92c0726ea0db55017
mauvetech <micson774@gmail.com>
2022-06-18 23:15:30 +0200

    test breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,8 @@ module.exports = {
 
       "2xl": "2536px",
       // => @media (min-width: 1536px) { ... }
+
+      "3xl": "4000px",
     },
   },
   plugins: [require("tailwind-scrollbar-hide")],
